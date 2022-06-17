9be293ae1f6663b55ed05ecdb853c4b5ee246bdb
Milad <javad.alghosi7904@gmail.com>
2022-06-17 03:26:05 -0700

    add request for search movie - add useDebounced hook

---
+++ hooks/useDebounce.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay: number = 5000): string => {
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
