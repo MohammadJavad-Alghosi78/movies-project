a7242c18f260dc2e42d1b9eb9e26dea2ab8706cb
Milad <javad.alghosi7904@gmail.com>
2022-06-19 13:53:13 -0700

    add husky

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+export default {
+    // Run type-check on changes to TypeScript files
+    "**/*.ts?(x)": () => "yarn type-check",
+    // Run ESLint on changes to JavaScript/TypeScript files
+    "**/*.(ts|js)?(x)": (filenames) => `yarn lint . ${filenames.join(" ")}`,
+};
