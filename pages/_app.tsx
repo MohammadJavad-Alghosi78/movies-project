3a1b28e3d45814c620ff0c0eada5c02289a0488c
Milad <javad.alghosi7904@gmail.com>
2022-06-19 13:29:22 -0700

    add prettier and eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // node_modules
+import React from "react";
 import type { AppProps } from "next/app";
 import { ComponentType, ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
@@ -9,29 +10,28 @@ import { store } from "../apps/shared/core/redux/api/store";
 import "apps/shared/styles/globals.css";
 import Head from "next/head";
 
-export type Page<P = {}> = NextPage<P> & {
-  // You can disable whichever y,sou don't need
-  getLayout?: (page: ReactElement) => ReactNode;
-  layout?: ComponentType;
+export type Page<P = Record<string, unknown>> = NextPage<P> & {
+    // You can disable whichever y,sou don't need
+    getLayout?: (page: ReactElement) => ReactNode;
+    layout?: ComponentType;
 };
 
 type Props = AppProps & {
-  Component: Page;
-  pageProps: any;
+    Component: Page;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    pageProps: any;
 };
 
 function MyApp({ Component, pageProps }: Props): JSX.Element {
-  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-  return (
-    <>
-      <Head>
-        <title>MOVIES PROJECT</title>
-      </Head>
-      <Provider store={store}>
-        {getLayout(<Component {...pageProps} />)}
-      </Provider>
-    </>
-  );
+    const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+    return (
+        <>
+            <Head>
+                <title>MOVIES PROJECT</title>
+            </Head>
+            <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+        </>
+    );
 }
 
 export default MyApp;
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,13 +1,13 @@
 // node_modules
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 const Search = (): null => {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/");
-  }, [router]);
-  return null;
+    const router = useRouter();
+    useEffect(() => {
+        router.push("/");
+    }, [router]);
+    return null;
 };
 
 export default Search;
