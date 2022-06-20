902b2fc28ad146dd06d3875cb4b45ab1a495afaf
Milad <javad.alghosi7904@gmail.com>
2022-06-20 00:58:20 -0700

    improve performance of movie page

--- apps/Movie/components/Movie.tsx
+++ apps/Movie/components/Movie.tsx
@@ -1,5 +1,5 @@
 // node_modules
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 // api
@@ -26,58 +26,64 @@ const MovieView = (): JSX.Element => {
     }, []);
 
     let content = <></>;
-    if (router.query.movieId) {
-        const { isLoading, isError, data: movie } = useGetMovieQuery(Number(router.query.movieId));
-        const { data: actors } = useGetCreditsQuery(Number(router.query.movieId));
-        const [addMovieToWatchList] = useAddMovieToWatchListMutation();
-        if (isLoading) content = <h1>Loading...</h1>;
-        if (isError) content = <h1>An error has been occured</h1>;
-        content = (
-            <>
-                <Box>
-                    <h3>{movie?.original_title}</h3>
-                    {isLogin && (
-                        <Button
-                            styles={{ width: "200px" }}
-                            onClick={() =>
-                                addMovieToWatchList({
-                                    media_type: "movie",
-                                    media_id: String(movie?.id),
-                                    watchlist: true,
-                                })
-                            }
-                        >
-                            Add to my watchlist
-                        </Button>
-                    )}
-                    <h4>{movie?.overview}</h4>
-                    <div className={classes.genre_wrapper}>
-                        <span>Genres:</span>
-                        <div>
-                            {movie?.genres?.map((genre: MovieGenreType) => (
-                                <Genre key={genre.id} label={genre.name} />
-                            ))}
-                        </div>
+    const {
+        isLoading,
+        isError,
+        data: movie,
+    } = useGetMovieQuery(Number(router.query.movieId), {
+        skip: !router.query.movieId,
+    });
+    const { data: actors } = useGetCreditsQuery(Number(router.query.movieId), {
+        skip: !router.query.movieId,
+    });
+    const [addMovieToWatchList] = useAddMovieToWatchListMutation();
+    if (isLoading) content = <h1>Loading...</h1>;
+    if (isError) content = <h1>An error has been occured</h1>;
+    content = (
+        <>
+            <Box>
+                <h3>{movie?.original_title}</h3>
+                {isLogin && (
+                    <Button
+                        styles={{ width: "200px" }}
+                        onClick={() =>
+                            addMovieToWatchList({
+                                media_type: "movie",
+                                media_id: String(movie?.id),
+                                watchlist: true,
+                            })
+                        }
+                    >
+                        Add to my watchlist
+                    </Button>
+                )}
+                <h4>{movie?.overview}</h4>
+                <div className={classes.genre_wrapper}>
+                    <span>Genres:</span>
+                    <div>
+                        {movie?.genres?.map((genre: MovieGenreType) => (
+                            <Genre key={genre.id} label={genre.name} />
+                        ))}
                     </div>
-                </Box>
-                <h1>Actors:</h1>
-                <div className={classes.actor_wrapper}>
-                    {actors?.cast.map((actor: any) => (
-                        // Question:
-                        // 1. Why this link does not work when a tag is not exist?
-                        <Link
-                            key={actor.id}
-                            href={`/movies/${router.query.movieId}/actor/${actor.credit_id}`}
-                        >
-                            <a>
-                                <ActorCard character={actor.character} name={actor.name} />
-                            </a>
-                        </Link>
-                    ))}
                 </div>
-            </>
-        );
-    }
+            </Box>
+            <h1>Actors:</h1>
+            <div className={classes.actor_wrapper}>
+                {actors?.cast.map((actor: any) => (
+                    // Question:
+                    // 1. Why this link does not work when a tag is not exist?
+                    <Link
+                        key={actor.id}
+                        href={`/movies/${router.query.movieId}/actor/${actor.credit_id}`}
+                    >
+                        <a>
+                            <ActorCard character={actor.character} name={actor.name} />
+                        </a>
+                    </Link>
+                ))}
+            </div>
+        </>
+    );
 
     return content;
 };
