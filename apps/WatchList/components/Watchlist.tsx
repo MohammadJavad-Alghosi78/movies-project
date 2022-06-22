4ab09b93ccbc43dcbd39344d24ea7fa636ab3156
Milad <javad.alghosi7904@gmail.com>
2022-06-22 07:42:29 -0700

    Add a message in watchlist page for logout state

--- apps/WatchList/components/Watchlist.tsx
+++ apps/WatchList/components/Watchlist.tsx
@@ -9,10 +9,12 @@ import WatchListMovieCard from "apps/WatchList/components/WatchListMovieCard";
 // styles
 import classes from "apps/WatchList/styles/style.module.scss";
 
-// Create MovielIst component and use it in wathclist and home page router ***
 function WatchListView() {
     const { isLoading, isError, data: movies } = useGetWatchListQuery();
+
     if (isLoading) return <h3>Loading...</h3>;
+    if (isError && !sessionStorage.getItem("token"))
+        return <h1>You are not login</h1>;
     if (isError) return <h3>An error has been occured</h3>;
     return (
         <div className={classes.watchlist_wrapper}>
