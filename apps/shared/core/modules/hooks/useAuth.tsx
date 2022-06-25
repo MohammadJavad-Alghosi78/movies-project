8885e04c0e615f73f2e6ee41ec47147a28ad314d
Milad <javad.alghosi7904@gmail.com>
2022-06-25 08:27:00 -0700

    Fix checking isLoggedIn flag in header component

--- apps/shared/core/modules/hooks/useAuth.tsx
+++ apps/shared/core/modules/hooks/useAuth.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 // types
 import { AuthType } from "apps/shared/types/modules/hooks/AuthType";
 
 const useAuth = (): AuthType => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
     const [sessionId, setSessionId] = useState<string | null>(null);
-    useEffect(() => {
-        const token = sessionStorage.getItem("token");
-        if (token) {
-            setIsLoggedIn(true);
-            setSessionId(token);
-        }
-    }, []);
-    const removeToken = () => {
-        sessionStorage.removeItem("token");
-        setIsLoggedIn(false);
-    };
-    const handleLogin = () => {
+    const handleLogin = useCallback(() => {
         const token = sessionStorage.getItem("token");
         if (token) {
             setIsLoggedIn(true);
         } else {
             setIsLoggedIn(false);
         }
+    }, []);
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if (token) {
+            setIsLoggedIn(true);
+            setSessionId(token);
+        }
+    }, [handleLogin]);
+    const removeToken = () => {
+        sessionStorage.removeItem("token");
+        setIsLoggedIn(false);
     };
     return { isLoggedIn, removeToken, handleLogin, sessionId };
 };
